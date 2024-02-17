2fd08add653207676dba59ab56395dfde0c78803
himanshu <himanshukumar75655893@gmail.com>
2024-02-17 22:19:54 +0530

    01_BASIC_OF_NUMPY.py modified

--- 01_BASIC_OF_NUMPY.py
+++ 01_BASIC_OF_NUMPY.py
@@ -1,3 +1,23 @@
 import numpy as np
+import math
 
 print(np.__version__)
+print(math.sqrt(4))
+
+a = complex(3,4)
+b = complex(5,6)
+print(type(a+b))
+
+
+# Need of Numpy 
+'''
+1. Creation of Array/Matrices
+2. Perform several operations on arrays/matrices
+3. Perform integral calculus operations
+4. Solving Differential equations
+5. Statistics related operations etc. 
+
+Too perform these comlex mathematical operatons, python does not contain any inbuilt library. 
+
+To perform these complex operations we require a library which is nothing but numpy.
+'''

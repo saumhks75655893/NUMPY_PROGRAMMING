10eef81b15ff19cff3d58d066b3039a21c07ce08
himanshu <himanshukumar75655893@gmail.com>
2024-02-22 01:16:53 +0530

    03_-1_random_numbers.py modified

--- Numpy programming/03_-1_random_numbers.py
+++ Numpy programming/03_-1_random_numbers.py
@@ -1,1 +1,37 @@
+# RANDOM : ----------
+
 import numpy as np 
+
+''' (i)->   rand() -----  '''
+
+# a = np.random.rand(4)
+# print(a)
+
+
+# print()
+# a1 = np.random.rand(2,4)
+# print(a1)
+
+''' (ii) -> randn() ------- '''
+
+# a = np.random.randn(4)
+# print(a)
+
+# b = np.random.randn(2,4)
+# print(b)
+
+''' (iii) -> ranf() --------- ''' 
+
+a = np.random.ranf(4)
+print(a)
+print()
+b = np.random.ranf(21)
+print(b)
+
+''' (iv) -> randint() --------- '''
+
+a = np.random.randint(1,10,10)
+print(a)
+print()
+b = np.random.randint(0,9,10)
+print(b)
